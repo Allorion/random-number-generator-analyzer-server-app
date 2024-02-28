83cd2a642d58925af19d14e1dba27dc90e6fe14f
Allori <wlad70657@gmail.com>
2024-02-28 19:25:15 +0300

    Исправил построение байтовых графиков в "Графическом анализе" Добавил построение битовых графиков в "Графическом анализе"

--- src/graphical-distribution-test-on-a-plane/apiGraphicalDistributionTestOnPlane.ts
+++ src/graphical-distribution-test-on-a-plane/apiGraphicalDistributionTestOnPlane.ts
@@ -1,26 +1,26 @@
 import express from "express";
 import {FILE_DIRECTORY} from "../global-elements/fileDirectory";
-import {readBitsFromFile} from "../global-elements/functions/readTextFromFile";
+import {readBitsFromFile, readTextFromFile} from "../global-elements/functions/readTextFromFile";
 import {Canvas, createCanvas} from "canvas";
-import {IPostDataStackOfBooks} from "../stack-of-books-test/types/TypeStackOfBooks";
 
 interface IReqData {
     nameFile: string,
     bitCount: number,
-    zoom: 1 | 2 | 3 | 4
+    zoom: 1 | 2 | 3 | 4,
+    bitFlag: boolean,
 }
 
 const router = express.Router()
 
 const generateImg = async (data: IReqData): Promise<Canvas> => {
 
-    // Читаем биты из файла, используя функцию readBitsFromFile
-    const buffer = readBitsFromFile(0, data.bitCount, FILE_DIRECTORY + data.nameFile);
-
     const zoom = data.zoom
 
-    const width: number = 255 * zoom
-    const height: number = 255 * zoom
+    let size = Math.floor(Math.sqrt(data.bitCount))
+    let count = size * size
+
+    const width: number = data.bitFlag ? size : 256 * zoom
+    const height: number = data.bitFlag ? size : 256 * zoom
 
     const canvas = createCanvas(width, height);
 
@@ -35,23 +35,53 @@ const generateImg = async (data: IReqData): Promise<Canvas> => {
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 1;
 
-    buffer.forEach((opt, index) => {
-        if (index % 2 === 1) {
-            const x = opt;
-            const y = buffer[index - 1];
-            // Начинаем новый путь
-            ctx.beginPath();
-            // Рисуем окружность с центром в координатах точки и радиусом 1 пикселей
-            ctx.arc(x * zoom, y * zoom, 1, 0, Math.PI * 2);
-            // Закрываем путь
-            ctx.closePath();
-            // Обводим путь
-            ctx.stroke();
+    if (data.bitFlag) {
+
+        let bitsLine: number[] = await readTextFromFile(0, count, data.nameFile, FILE_DIRECTORY)
+
+        let x: number = size - 1
+        let y: number = 0
+
+        for (let i: number = 0; i < count; i++) {
+
+            ctx.fillStyle = bitsLine[i] === 1 ? "black" : 'white';
+
+            ctx.fillRect(x, y, 1, 1); // Задать ширину и высоту пикселя равными 1
+
+            if (x === size - 1) {
+                y++
+                x = 0
+            } else {
+                x++
+            }
         }
-    })
 
-    return canvas
+        return canvas
+    } else {
+        // Читаем биты из файла, используя функцию readBitsFromFile
+        const buffer = readBitsFromFile(0, data.bitCount, FILE_DIRECTORY + data.nameFile);
 
+        buffer.forEach((opt, index) => {
+            if (index % 2 === 1) {
+                const x = opt;
+                const y = buffer[index - 1];
+
+                ctx.fillStyle = "black"
+                ctx.fillRect(x * zoom, y * zoom, 1, 1);
+
+                // // Начинаем новый путь
+                // ctx.beginPath();
+                // // Рисуем окружность с центром в координатах точки и радиусом 1 пикселей
+                // ctx.arc(x * zoom, y * zoom, 1, 0, Math.PI * 2);
+                // // Закрываем путь
+                // ctx.closePath();
+                // // Обводим путь
+                // ctx.stroke();
+            }
+        })
+
+        return canvas
+    }
 }
 
 const checkReqData = (elem: IReqData, warning: string[]) => {
@@ -68,7 +98,7 @@ const checkReqData = (elem: IReqData, warning: string[]) => {
     if (elem.bitCount < 100) {
         warning.push(`Количество бит последовательности должно быть не менее 100`)
     }
-    if (elem.bitCount > 1000000) {
+    if (elem.bitCount > 1006000000) {
         warning.push(`Количество бит последовательности должно быть не более 1млн`)
     }
     if (elem.zoom < 0) {
